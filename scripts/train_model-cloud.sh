eafef9616e946c3d3debf3942ab2a183f5e4d3c7
Kelvin Foster <kelvinfosterdk@gmail.com>
2022-08-22 14:31:42 +0200

    asdflkasjdfkl

---
+++ scripts/train_model-cloud.sh
@@ -0,0 +1,15 @@
+PROJECT_ID=better-mldtu
+BUCKET_NAME=better-mldtu-aiplatform
+REGION=europe-west1
+IMAGE_REPO_NAME=mlops_pytorch_custom_container
+IMAGE_TAG=mlops_pytorch_gpu
+IMAGE_URI=gcr.io/better-mldtu/mlops_pytorch_custom_container:mlops_pytorch_gpu
+MODEL_DIR=pytorch_model_$(date +%Y%m%d_%H%M%S)
+JOB_NAME=custom_container_job_$(date +%Y%m%d_%H%M%S)
+
+gcloud ai-platform jobs submit training ${JOB_NAME} \                     
+  --scale-tier BASIC_GPU \
+  --region ${REGION} \
+  --master-image-uri ${IMAGE_URI} \
+  -- \
+  --model-dir="gs://$BUCKET_NAME/$MODEL_DIR"
