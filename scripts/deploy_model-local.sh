caeafbe114efc02a68c158ee20da4b7d15d2695d
Kelvin Foster <kelvinfosterdk@gmail.com>
2022-09-05 17:12:10 +0200

    hej nicki

--- scripts/deploy_model-local.sh
+++ scripts/deploy_model-local.sh
@@ -5,7 +5,7 @@ IMG_NAME=test_img
 echo $IMG_NAME
 IMG_TAG=gcr.io/better-mldtu/mlops_pytorch_custom_container:$IMG_NAME
 docker build --build-arg LATEST_MODEL --tag $IMG_TAG -f Dockerfile_deploy .
+docker run -d -p 8080:8080 --name=local_imdb test_img
 
-docker push $IMG_TAG
-#docker run -d -p 8080:8080 --name=local_imdb test_img
+
 
